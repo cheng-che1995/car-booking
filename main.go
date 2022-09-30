75346b7b034f5cb1ab566b1e80282a382a373085
smdick1995 <smdick1995@gmail.com>
2022-09-30 15:29:35 +0800

    basic

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type DogString string
+type jwtCustomClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
 
 // type CustomFunc func(echo.Context) error
 var users map[string]string = map[string]string{
@@ -16,42 +24,51 @@ var users map[string]string = map[string]string{
 	"wilson": "1234",
 }
 
-func customHandler(s string) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		return c.String(http.StatusOK, s)
-	}
-}
-
 func handle(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World!")
-}
-
-func handle2(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello Sec!")
 }
 
 func login(c echo.Context) error {
 	username := c.FormValue("username")
 	password := c.FormValue("password")
 
-	pw, doesKeyExist := users[username]
+	pw, ok := users[username]
 
-	if pw != password || !doesKeyExist {
+	if pw != password || !ok {
 		return echo.ErrUnauthorized
 	}
 
-	return c.String(http.StatusOK, fmt.Sprintf("username:%s\npassword:%s\n", username, password))
+	claims := &jwtCustomClaims{
+		username,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, LoginResponse{t})
 }
 
 func main() {
-	fmt.Println("jghfghfdgfs")
-	fmt.Println("jghfghgggggg")
-	fmt.Printf("hello %s %d\n", "cat", 5)
-
 	// var e *echo.Echo // = echo.New()
 	e := echo.New()
-	e.GET("/dog", handle)
 	e.POST("/login", login)
+
+	b := e.Group("/booking")
+	b.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte("secret"),
+		ErrorHandlerWithContext: func(err error, c echo.Context) error {
+			return c.String(http.StatusUnauthorized, "JWT驗證無效")
+		},
+	}))
+
 	e.Use(middleware.Logger())
 	e.Logger.Fatal(e.Start(":1323"))
 
