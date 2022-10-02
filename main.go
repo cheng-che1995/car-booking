15ff05d04c4701e9969e1d6998b1855ffd1d221c
smdick1995 <smdick1995@gmail.com>
2022-10-02 22:21:13 +0800

    add appointments route and basic func

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ type LoginResponse struct {
 	Token string `json:"keyyy"`
 }
 
+type AppointmentsResponse struct {
+	Appointments []time.Time `json:"appointments"`
+}
+
 // type CustomFunc func(echo.Context) error
 var users map[string]string = map[string]string{
 	"tony":   "tonytsai",
@@ -55,20 +59,35 @@ func login(c echo.Context) error {
 	return c.JSON(http.StatusOK, LoginResponse{t})
 }
 
+func createAppointments(c echo.Context) error {
+	selectedDate := c.FormValue("selectedDate")
+	t, err := time.Parse("2006-01-02", selectedDate)
+	if err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, "預約成功！您的預約日期為："+t.Format("2006-01-02"))
+}
+
+func getAppointments(c echo.Context) error {
+	return c.JSON(http.StatusOK, AppointmentsResponse{})
+}
+
 func main() {
 	// var e *echo.Echo // = echo.New()
 	e := echo.New()
 	e.POST("/login", login)
-
 	b := e.Group("/booking")
 	b.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		// Claims:     &jwtCustomClaims{},
 		Claims:     &jwtCustomClaims{},
 		SigningKey: []byte("secret"),
 		ErrorHandlerWithContext: func(err error, c echo.Context) error {
-			return c.String(http.StatusUnauthorized, "JWT驗證無效")
+			return c.String(http.StatusUnauthorized, err.Error())
 		},
 	}))
-
+	b.GET("/dog", handle)
+	b.POST("/appointments", createAppointments)
+	b.GET("/appointments", getAppointments)
 	e.Use(middleware.Logger())
 	e.Logger.Fatal(e.Start(":1323"))
 
