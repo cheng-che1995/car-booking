bf67d209e035f9076e79ce28624a3ca7846446e1
smdick1995 <smdick1995@gmail.com>
2022-10-03 01:41:42 +0800

    add cancelAppointments func

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ type AppointmentsResponse struct {
 	Appointments []time.Time `json:"appointments"`
 }
 
+var appointments []time.Time
+
 // type CustomFunc func(echo.Context) error
 var users map[string]string = map[string]string{
 	"tony":   "tonytsai",
@@ -65,11 +67,36 @@ func createAppointments(c echo.Context) error {
 	if err != nil {
 		return err
 	}
+	appointments = append(appointments, t)
 	return c.String(http.StatusOK, "預約成功！您的預約日期為："+t.Format("2006-01-02"))
 }
 
-func getAppointments(c echo.Context) error {
-	return c.JSON(http.StatusOK, AppointmentsResponse{})
+func searchAppointments(c echo.Context) error {
+	return c.JSON(http.StatusOK, AppointmentsResponse{Appointments: appointments})
+}
+
+func cancelAppointments(c echo.Context) error {
+	selectedDate := c.FormValue("selectedDate")
+	t, err := time.Parse("2006-01-02", selectedDate)
+	if err != nil {
+		return err
+	}
+	var found bool
+	for i := range appointments {
+		if t == appointments[i] {
+			found = true
+			for j := range appointments[i : len(appointments)-1] {
+				appointments[i+j] = appointments[i+j+1]
+			}
+			appointments = appointments[:len(appointments)-1]
+			break
+		}
+
+	}
+	if found {
+		return c.String(http.StatusOK, "取消成功！您已將"+t.Format("2006-01-02")+"預約取消！")
+	}
+	return c.String(http.StatusOK, "查無此預約！")
 }
 
 func main() {
@@ -87,7 +114,8 @@ func main() {
 	}))
 	b.GET("/dog", handle)
 	b.POST("/appointments", createAppointments)
-	b.GET("/appointments", getAppointments)
+	b.GET("/appointments", searchAppointments)
+	b.DELETE("/appointments", cancelAppointments)
 	e.Use(middleware.Logger())
 	e.Logger.Fatal(e.Start(":1323"))
 
