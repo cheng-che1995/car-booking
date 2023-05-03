98550c0cc3575d7578147dfd14b236b6e608685d
smdick1995 <smdick1995@gmail.com>
2023-05-04 00:25:17 +0800

    perf: add plate check func in models.go

--- models.go
+++ models.go
@@ -60,12 +60,14 @@ func (u User) CheckPassword() (bool, error) {
 	return true, nil
 }
 
+var ErrHashFailed = errors.New("密碼雜湊轉換失敗！")
+
 func (u User) HashPassword() ([]byte, error) {
 	hash := sha256.New()
 	hash.Write([]byte(u.Password))
 	marshaler, ok := hash.(encoding.BinaryMarshaler)
 	if !ok {
-		return nil, errors.New("hash password failed.")
+		return nil, ErrHashFailed
 	}
 	pwd, err := marshaler.MarshalBinary()
 	if err != nil {
@@ -74,15 +76,20 @@ func (u User) HashPassword() ([]byte, error) {
 	return pwd, nil
 }
 
+var (
+	ErrUsernameEmpty = errors.New("使用者名稱不得為空值！")
+	ErrPasswordEmpty = errors.New("密碼不得為空值！")
+)
+
 func (u *User) Validate() error {
 	if u == nil {
 		return nil
 	}
 	if u.Username == "" {
-		return errors.New("使用者名稱不得為空值！")
+		return ErrUsernameEmpty
 	}
 	if u.Password == "" {
-		return errors.New("密碼不得為空值！")
+		return ErrPasswordEmpty
 	}
 	return nil
 }
@@ -97,17 +104,37 @@ func (c *Car) GenerateUuid() {
 	c.Uuid = uuid.NewV4().String()
 }
 
-// TODO: 驗證車牌正確性
+// 註：車牌命名規範為 AAA-0001 ~ ZZZ-9999（實際上根據交通部定義，除了須以車牌號碼範圍區別車種，還有額外的規範，如：避免出現4444或是不雅字母組合等，在此忽略不計。）
 func (c *Car) Validate() error {
 	if c == nil {
 		return nil
 	}
-	if c.Plate == "" {
-		return errors.New("車牌不得為空值！")
-	}
 	if c.UserUuid == "" {
 		return errors.New("UserUuid不得為空值！")
 	}
+	if c.Plate == "" {
+		return errors.New("車牌不得為空值！")
+	}
+	if len(c.Plate) != 8 {
+		return errors.New("車牌長度不符合命名規範！")
+	}
+	for _, r := range c.Plate {
+		switch r {
+		case 0, 1, 2:
+			if !unicode.IsLetter(r) || !unicode.IsUpper(r) {
+				return errors.New("車牌格式不符合命名規範！")
+			}
+		case 3:
+			if r != '-' {
+				return errors.New("車牌格式不符合命名規範！")
+			}
+		default:
+			if !unicode.IsDigit(r) {
+				return errors.New("車牌格式不符合命名規範！")
+			}
+		}
+	}
+
 	return nil
 }
 
